ea7ff70026fd9725d5736c462b32b6121bbe8c5b
Lucas <lucasleeeeeeeee@gmail.com>
2020-01-14 10:14:06 +0800

    ResourceManagementFrom: InitializeComponent: Properly define type_combo
    Somehow, either me or visual studio fucked up the definition and I was unable to access the combobox. Fix this by manully fixing the definition.

--- ResourceManagementForm.Designer.cs
+++ ResourceManagementForm.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.ComboBox typecombo;
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -38,78 +37,86 @@
             this.update_button = new System.Windows.Forms.Button();
             this.add_button = new System.Windows.Forms.Button();
             this.back_button = new System.Windows.Forms.Button();
-            typecombo = new System.Windows.Forms.ComboBox();
+            this.type_combo = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // type_combo
+            // 
+            this.type_combo.FormattingEnabled = true;
+            this.type_combo.Location = new System.Drawing.Point(140, 24);
+            this.type_combo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.type_combo.Name = "type_combo";
+            this.type_combo.Size = new System.Drawing.Size(92, 21);
+            this.type_combo.TabIndex = 3;
+            this.type_combo.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(309, 9);
+            this.label1.Location = new System.Drawing.Point(232, 7);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(155, 17);
+            this.label1.Size = new System.Drawing.Size(118, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Resource Management";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 33);
+            this.label2.Location = new System.Drawing.Point(9, 27);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(95, 17);
+            this.label2.Size = new System.Drawing.Size(71, 13);
             this.label2.TabIndex = 1;
             this.label2.Text = "Filter By Type";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 73);
+            this.label3.Location = new System.Drawing.Point(9, 59);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(87, 17);
+            this.label3.Size = new System.Drawing.Size(65, 13);
             this.label3.TabIndex = 2;
             this.label3.Text = "Filter by Skill";
-            // 
-            // typecombo
-            // 
-            typecombo.FormattingEnabled = true;
-            typecombo.Location = new System.Drawing.Point(187, 30);
-            typecombo.Name = "typecombo";
-            typecombo.Size = new System.Drawing.Size(121, 24);
-            typecombo.TabIndex = 3;
-            typecombo.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // skillcombo
             // 
             this.skillcombo.FormattingEnabled = true;
-            this.skillcombo.Location = new System.Drawing.Point(187, 70);
+            this.skillcombo.Location = new System.Drawing.Point(140, 57);
+            this.skillcombo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.skillcombo.Name = "skillcombo";
-            this.skillcombo.Size = new System.Drawing.Size(121, 24);
+            this.skillcombo.Size = new System.Drawing.Size(92, 21);
             this.skillcombo.TabIndex = 4;
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 121);
+            this.dataGridView1.Location = new System.Drawing.Point(9, 98);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(776, 287);
+            this.dataGridView1.Size = new System.Drawing.Size(582, 233);
             this.dataGridView1.TabIndex = 5;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(692, 414);
+            this.button2.Location = new System.Drawing.Point(519, 336);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(96, 36);
+            this.button2.Size = new System.Drawing.Size(72, 29);
             this.button2.TabIndex = 7;
             this.button2.Text = "Delete";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // update_button
             // 
-            this.update_button.Location = new System.Drawing.Point(590, 414);
+            this.update_button.Location = new System.Drawing.Point(442, 336);
+            this.update_button.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.update_button.Name = "update_button";
-            this.update_button.Size = new System.Drawing.Size(96, 36);
+            this.update_button.Size = new System.Drawing.Size(72, 29);
             this.update_button.TabIndex = 8;
             this.update_button.Text = "Update";
             this.update_button.UseVisualStyleBackColor = true;
@@ -117,9 +124,10 @@
             // 
             // add_button
             // 
-            this.add_button.Location = new System.Drawing.Point(488, 414);
+            this.add_button.Location = new System.Drawing.Point(366, 336);
+            this.add_button.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.add_button.Name = "add_button";
-            this.add_button.Size = new System.Drawing.Size(96, 36);
+            this.add_button.Size = new System.Drawing.Size(72, 29);
             this.add_button.TabIndex = 9;
             this.add_button.Text = "Add";
             this.add_button.UseVisualStyleBackColor = true;
@@ -127,9 +135,10 @@
             // 
             // back_button
             // 
-            this.back_button.Location = new System.Drawing.Point(12, 414);
+            this.back_button.Location = new System.Drawing.Point(9, 336);
+            this.back_button.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.back_button.Name = "back_button";
-            this.back_button.Size = new System.Drawing.Size(82, 36);
+            this.back_button.Size = new System.Drawing.Size(62, 29);
             this.back_button.TabIndex = 10;
             this.back_button.Text = "BACK";
             this.back_button.UseVisualStyleBackColor = true;
@@ -137,19 +146,20 @@
             // 
             // ResourceManagementForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
             this.Controls.Add(this.back_button);
             this.Controls.Add(this.add_button);
             this.Controls.Add(this.update_button);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.skillcombo);
-            this.Controls.Add(typecombo);
+            this.Controls.Add(type_combo);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "ResourceManagementForm";
             this.Text = "ASEAN SKILLS 2020";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
@@ -164,6 +174,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.ComboBox skillcombo;
+        private System.Windows.Forms.ComboBox type_combo
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button update_button;
